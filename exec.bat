d8f4d0cafc52121903ae2bc215c46e7534027fb9
xhakohnnj <xhakohnnj@gmail.com>
2023-06-29 00:43:08 +0900

    バッチあげとく。

--- exec.bat
+++ exec.bat
@@ -1,8 +1,13 @@
 echo off
 
+rem XLSX版を実行します.
+rem こちらがうまくいかない場合は下方にあるCSV版をお試しください.
 rem タイトルリリース(開始、終了), ゲームパス(開始、終了), ゲームイベント(開始、終了)
-py generateTennpure.py output.txt 2023/06/01 2023/12/31 2023/06/15 2023/12/31 2023/06/01 2023/12/31
-rem py generateTennpureWithFile.py output.txt release.ics gamepass_in.ics gamepass_out.ics 2022/11/22 2023/03/31 2022/11/22 2023/03/31
+py generateTennpureXlsx.py output.txt 2023/06/01 2023/09/30 2023/06/15 2023/12/31 2023/06/30 2023/07/01
+
+rem CSV版を実行します.
+rem タイトルリリース(開始、終了), ゲームパス(開始、終了), ゲームイベント(開始、終了)
+rem py generateTennpureCsv.py output.txt 2023/06/01 2023/12/31 2023/06/15 2023/12/31 2023/06/01 2023/12/31
 
 pause
 
