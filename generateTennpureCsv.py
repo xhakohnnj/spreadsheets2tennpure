192af403add784ef090d1b93d6a84496ddc86b3d
xhakohnnj <xhakohnnj@gmail.com>
2023-07-09 14:07:12 +0900

    元データのファイル名を変えたのを反映してなかった。

--- generateTennpureCsv.py
+++ generateTennpureCsv.py
@@ -19,13 +19,13 @@ class Args(IntEnum):
     GAME_EVENTS_DATE_END        = auto()    # ゲームイベントの日付の範囲(終了)
 
 # リリースカレンダー
-release_list = CsvToReleaseDataList.FromFile( 'タイトルリリース - タイトルリリース.csv', sys.argv[Args.TITLE_RELEASE_DATE_START], sys.argv[Args.TITLE_RELEASE_DATE_END] )
+release_list = CsvToReleaseDataList.FromFile( 'Xboxタイトルリリース - タイトルリリース.csv', sys.argv[Args.TITLE_RELEASE_DATE_START], sys.argv[Args.TITLE_RELEASE_DATE_END] )
 # ゲームパス IN
-gamepass_in_list = CsvToReleaseDataList.FromFile( 'タイトルリリース - ゲームパスIN.csv', sys.argv[Args.GAME_PASS_DATE_START], sys.argv[Args.GAME_PASS_DATE_END] )
+gamepass_in_list = CsvToReleaseDataList.FromFile( 'Xboxタイトルリリース - ゲームパスIN.csv', sys.argv[Args.GAME_PASS_DATE_START], sys.argv[Args.GAME_PASS_DATE_END] )
 # ゲームパス OUT
-gamepass_out_list = CsvToReleaseDataList.FromFile( 'タイトルリリース - ゲームパスOUT.csv', sys.argv[Args.GAME_PASS_DATE_START], sys.argv[Args.GAME_PASS_DATE_END] )
+gamepass_out_list = CsvToReleaseDataList.FromFile( 'Xboxタイトルリリース - ゲームパスOUT.csv', sys.argv[Args.GAME_PASS_DATE_START], sys.argv[Args.GAME_PASS_DATE_END] )
 # ゲームイベント
-gameevents_list = CsvToReleaseDataList.FromFile( 'タイトルリリース - イベント.csv', sys.argv[Args.GAME_EVENTS_DATE_START], sys.argv[Args.GAME_EVENTS_DATE_END] )
+gameevents_list = CsvToReleaseDataList.FromFile( 'Xboxタイトルリリース - イベント.csv', sys.argv[Args.GAME_EVENTS_DATE_START], sys.argv[Args.GAME_EVENTS_DATE_END] )
 
 with open( sys.argv[Args.OUTPUT_FILE], mode='w', encoding='utf-8', newline=os.linesep ) as output_file:
     TennpureGenerate.ToFile( output_file, release_list, gamepass_in_list, gamepass_out_list, gameevents_list )
